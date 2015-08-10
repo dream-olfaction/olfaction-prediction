dcbfe8a92ad20c453e59844ef262e40bee3e6e1b
Kurt De Grave <kurt.degrave@cs.kuleuven.be>
2015-08-10 15:11:07 +0200

    compute NPSDK features

--- opc_python/degrave/download_mol.py
+++ opc_python/degrave/download_mol.py
@@ -8,17 +8,16 @@ olfaction_prediction_path = os.path.split(os.path.split(curr_path)[0])[0]
 sys.path.append(olfaction_prediction_path)
 import opc_python
 
-from opc_python import * # Import constants.  
+# from opc_python import * # Import constants.  
 from opc_python.utils.loading import get_CIDs
 
-allCIDs = sorted(get_CIDs('training') + get_CIDs('leaderboard') + get_CIDs('testset'))
-
 mol_path = olfaction_prediction_path + '/data/sdf/'
-if not os.path.isdir(mol_path):
-  os.mkdir(mol_path)
+
+def get_all_CIDs():
+  return sorted(get_CIDs('training') + get_CIDs('leaderboard') + get_CIDs('testset'))
 
 #for cid in allCIDs:
-  #os.exec('wget -O individual-sdfs/${CID}.sdf http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/${CID}/record/SDF/?record_type=2d&response_type=save')
+#  os.exec('wget -O individual-sdfs/'+cid+'.sdf http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/'+cid+'/record/SDF/?record_type=2d&response_type=save')
 
 def download_SDF(cid):
   url = 'http://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/'+str(cid)+'/record/SDF/?record_type=2d&response_type=save'
@@ -33,5 +32,32 @@ def download_SDF_to_file(cid):
   with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
     shutil.copyfileobj(response, out_file)
 
-for cid in allCIDs:
-  download_SDF_to_file(cid)
+# This takes a minute or so
+def download_all_mol():
+  if not os.path.isdir(mol_path):
+    os.mkdir(mol_path)
+  with open(mol_path + 'all_mol.sdf', 'w') as accumulator_file:
+    for cid in get_all_CIDs():
+      print('cid: ' + str(cid))
+      file_name = mol_path + 'mol' + str(cid) + '.sdf'
+      sdfStr = download_SDF(cid)
+      with open(file_name, 'w') as outfile:
+        outfile.write(sdfStr)
+        accumulator_file.write(sdfStr)
+
+# download_all_mol()
+
+class Mol:
+  def __init__(self):
+    pass
+  
+  def __iter__(self):
+    self.CID_iter = get_all_CIDs().__iter__()
+    return self
+
+  def __next__(self):
+    cid = self.CID_iter.next()
+    file_name = mol_path + 'mol' + str(cid) + '.sdf'
+    with open(file_name, 'r') as SDfile:
+      content = SFfile.read()
+    return content
