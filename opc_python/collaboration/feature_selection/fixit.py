cd01ef3fde427bdf755668fa806318341ec7d6cb
Kurt De Grave <kurt.degrave@cs.kuleuven.be>
2015-09-24 14:13:43 +0200

    fix: NSPDK kernel values were permuted

--- opc_python/collaboration/feature_selection/fixit.py
+++ opc_python/collaboration/feature_selection/fixit.py
@@ -1,12 +1,22 @@
+#/usr/bin/python3
+
 import pandas as pd
 
-at = pd.read_csv("all_features_training_nonspdk.csv.tmp")
-av = pd.read_csv("all_features_validation_nonspdk.csv.tmp")
-nc = pd.read_csv("nspdk_gram_cid.csv.tmp")
-merged_t = at.merge(nc, on='CID')
-merged_t.to_csv("all_features_training_fixed.csv", index=False)
-merged_v = av.merge(nc, on='CID')
-merged_v.to_csv("all_features_validation_fixed.csv", index=False)
+train = pd.read_csv("all_features_training.csv")
+valid = pd.read_csv("all_features_validation.csv")
 
-# The sum of the file sizes is not the same... 
-# Some problem with missing data?
+if train.columns[[7307]] != 'nspdk1':
+  raise Exception('The input file doesn not seem to contain the error. Maybe it has already been fixed?')
+
+# delete the permuted nspdk columns
+train=train.drop(train.columns[list(range(7307,9744))], axis=1)
+valid=valid.drop(valid.columns[list(range(7307,9744))], axis=1)
+
+# add correct nspdk columns
+# (Note that these are NPSDK kernel values, i.e. similarities with other molecules, not NPSDK features.)
+nc = pd.read_csv("nspdk_gram_cid.csv.tmp")
+train = train.merge(nc, on='CID')
+valid = valid.merge(nc, on='CID')
+
+train.to_csv("all_features_training_fixed.csv", index=False)
+valid.to_csv("all_features_validation_fixed.csv", index=False)
--- opc_python/collaboration/feature_selection/fixit.sh
+++ opc_python/collaboration/feature_selection/fixit.sh
@@ -1,9 +1,14 @@
+#/bin/bash
 # Leander's files all_features_*.csv contain the NSPDK features for the wrong CIDs.
-# This is an attempt to fix those files... it doesn't work yet.
-for SET in training validation; do
-  cat all_features_${SET}.csv | cut -f 1-7307,9745- -d , > all_features_${SET}_nonspdk.csv.tmp
-  cut -f 1 -d , all_features_${SET}.csv >cid_${SET}.csv.tmp
-done
+# This is an attempt to fix those files...
+# This may cause problems for missing values and NaNs.
+
+# This is now done in Python
+# for SET in training validation; do
+#   cat all_features_${SET}.csv | cut -f 1-7307,9745- -d , > all_features_${SET}_nonspdk.csv.tmp
+#   cut -f 1 -d , all_features_${SET}.csv >cid_${SET}.csv.tmp
+# done
+
 # Make a file with first the CID, then the NSPDK Gramian
 #   The header
 echo "CID,nspdk$(seq -s ",nspdk" 1 2437)" > nspdk_gram_cid.csv.tmp
@@ -13,9 +18,15 @@ zcat ../../../data/derived/nspdk_r3_d4_unaug_gramian.mtx.gz \
   | head -n 476 | tr " " , >> nspdk_gram_cid.csv.tmp
 
 python3 fixit.py
+
+rm nspdk_gram_cid.csv.tmp
+
 # The following is easier in Python...
 # tail -n +2 all_features_training_nonspdk.csv.tmp | sort -t , > all_features_training_nonspdk_noheader.csv.tmp
 # join --check-order -j 1 -t , all_features_training_nonspdk_noheader.csv.tmp nspdk_gram_cid.csv.tmp > all_features_training_fixed.csv
 # for SET in training validation; do
 #   join --check-order --header -j 1 -t , all_features_${SET}_nonspdk.csv.tmp nspdk_gram_cid.csv.tmp > all_features_${SET}_fixed.csv
 # done
+
+mv all_features_training_fixed.csv all_features_training.csv
+mv all_features_validation_fixed.csv all_features_validation.csv
